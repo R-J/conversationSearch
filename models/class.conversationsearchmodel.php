140f7fcd7f07d4c2190891fa98effed461f8787e
R-J <robin.jurinka@gmx.net>
2017-05-11 13:08:07 +0200

    Finished model class!
    Plugin is generally working. Might need some fine tuning.

--- models/class.conversationsearchmodel.php
+++ models/class.conversationsearchmodel.php
@@ -1,42 +1,97 @@
 <?php
 
-class ConversationSearchModel extends SearchModel {
+class ConversationSearchModel extends SearchModel { // Gdn_Pluggable {
+    /**  @var Gdn_Database Database object. */
+    public $Database;
+
+    /** @var Gdn_SQLDriver Contains the sql driver for the object. */
+    public $SQL;
+
+    /** @var boolean Restrict results to the conversations of one user */
+    public $checkPermission = true;
+
+    /** @var null|integer If checkPermission is true, restrict to this user. Session user will be used if this value isn't set */
+    public $conversationUserID = null;
+
     /**
+     * Class constructor.
      *
+     * @param boolean $checkPermission Restrict results based on user.
+     * @param null|integer $conversationUserID The user to use for restriction.
      *
-     * @param $search
-     * @param int $offset
-     * @param int $limit
-     * @return array|null
-     * @throws Exception
+     * @return  void.
+     */
+    public function __construct($checkPermission = true, $conversationUserID = null) {
+        // $this->Database = Gdn::database();
+        // $this->SQL = $this->Database->sql();
+
+        $this->checkPermission = $checkPermission;
+        if ($conversationUserID === null) {
+            $conversationUserID = Gdn::session()->UserID;
+        }
+        $this->conversationUserID = $conversationUserID;
+
+        parent::__construct();
+    }
+
+    /**
+     * Performs a boolean search.
+     *
+     * Allows using "+" (AND) and "-" (NOT) in search.
+     *
+     * @param string $search The string to look up.
+     * @param integer $offset The offset to start from (for pagination).
+     * @param integer $limit The limit of results (for pagination).
+     *
+     * @return array Search result.
      */
     public function search($search, $offset = 0, $limit = 20) {
+        // If a third party plugin wants to take influence on the source,
+        // this would be a possibility to change the search string.
+        // $this->EventArguments['Search'] = &$Search;
+        // $this->fireEvent('Search');
+
         // If there are no searches then return an empty array.
-        if (trim($search) == '') { // || count($this->_SearchSql) == 0) {
+        if (trim($search) == '') {
             return [];
         }
-        $searchMode = strtolower(c('Conversation.Search.Mode', 'matchboolean'));
 
-        // Perform the search by unioning all of the sql together.
+        // Optionally restrict results to one user.
+        if ($this->checkPermission != false) {
+            $this->SQL->where('uc.UserID', intval($this->conversationUserID));
+        }
+
+        // Build the search sql.
         $sql = $this->SQL
-            ->select('cm.Body', "match (%s) against(:search1 IN BOOLEAN MODE)", 'Relevance')
+            ->select('cm.Body', "match (%s) against (:search1 in boolean mode)", 'Relevance')
             ->select('cm.MessageID as PrimaryID, c.Subject as Title, cm.Body as Summary, cm.Format')
             ->select("'messages/', cm.ConversationID, '#Message_', cm.MessageID", 'concat', 'Url')
             ->select('cm.DateInserted')
             ->select('cm.InsertUserID as UserID')
             ->select("'Message'", '', 'RecordType')
+            ->select('u.Name, u.Photo, u.Email')
             ->from('ConversationMessage cm')
-            ->join('Conversation c', 'c.ConversationID = cm.ConversationID')
-            ->where("match(cm.Body) against (:search2  IN BOOLEAN MODE)", null, false, false)
+            ->join('Conversation c', 'cm.ConversationID = c.ConversationID', 'left outer')
+            ->join('UserConversation uc', 'uc.ConversationID = cm.ConversationID', 'left outer')
+            ->join('User u', 'cm.InsertUserID = u.UserID', 'left outer')
+            ->where('uc.Deleted', 0)
+            ->where('uc.DateLastViewed is not null')
+            ->where('cm.MessageID <=', 'uc.LastMessageID', true, false)
+            ->where("match(cm.Body) against (:search2 in boolean mode)", null, false, false)
             ->orderBy('cm.DateInserted', 'desc')
             ->limit($limit, $offset)
             ->getSelect();
 
-        $parameters = [':search1' => $search, ':search2' => $search];
-        $this->SQL->namedParameters($parameters);
+        // Third party plugins would be able to take influence on the query.
+        // $this->EventArguments['Sql'] = &$sql;
+        // $this->fireEvent('AfterBuildSearchQuery');
+
+        // Add two named parameters for our search and execute the query.
+        $this->SQL->namedParameter(':search1', false, $search);
+        $this->SQL->namedParameter(':search2', false, $search);
         $result = $this->SQL->query($sql)->resultArray();
 
-        // Transform Body To Summary.
+        // Condense Body To Summary.
         foreach ($result as $key => $value) {
             if (isset($value['Summary'])) {
                 $result[$key]['Summary'] = condense(Gdn_Format::to($value['Summary'], $value['Format']));
